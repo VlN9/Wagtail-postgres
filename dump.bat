9358441b6c5025048ca42d58312c50dbf2646e51
VlN9 <tron4ik.ne@gmail.com>
2022-07-27 17:43:21 +0300

    Fix container name

--- dump.bat
+++ dump.bat
@@ -1,9 +1,10 @@
-docker stop wagtail-compose-web-1 
+
+docker stop wagtail-postgres-web 
 timeout /t 05
-docker exec -i wagtail-compose-db-1 /bin/bash -c "dropdb -U demouser demo_wagtail"
+docker exec -i wagtail-postgres-db /bin/bash -c "dropdb -U demouser demo_wagtail"
 timeout /t 05
-docker exec -i wagtail-compose-db-1 /bin/bash -c "createdb -U demouser -T template0 demo_wagtail"
+docker exec -i wagtail-postgres-db /bin/bash -c "createdb -U demouser -T template0 demo_wagtail"
 timeout /t 1
-docker exec -i wagtail-compose-db-1 /bin/bash -c "pg_restore -U demouser -C -d demo_wagtail" < ./db.dump
+docker exec -i wagtail-postgres-db /bin/bash -c "pg_restore -U demouser -C -d demo_wagtail" < ./db.dump
 timeout /t 1
 docker compose up -d
--- dump.sh
+++ dump.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#docker exec -i wagtail-postgres-db-1 bin/bash -c "psql -U demouser demo_wagtail" < ./db.dump
+docker stop wagtail-postgres-web
 
-docker stop wagtail-postgres-web-1
+docker exec -i wagtail-postgres-db bin/bash -c "dropdb -U demouser demo_wagtail"
+docker exec -i wagtail-postgres-db bin/bash -c "createdb -U demouser -T template0 demo_wagtail"
 
-docker exec -i wagtail-postgres-db-1 bin/bash -c "dropdb -U demouser demo_wagtail"
-docker exec -i wagtail-postgres-db-1 bin/bash -c "createdb -U demouser -T template0 demo_wagtail"
-
-docker exec -i wagtail-postgres-db-1 bin/bash -c "pg_restore -U demouser -C -d demo_wagtail" < ./db.dump
+docker exec -i wagtail-postgres-db bin/bash -c "pg_restore -U demouser -C -d demo_wagtail" < ./db.dump
 docker compose up -d
